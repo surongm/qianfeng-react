f906bcde1e4381611d3b84bc5851ba07e104b70f
surongm <1137747302@qq.com>
2019-11-02 22:03:57 +0800

    组件可以嵌套组件

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
+// 组件是可以嵌套组件的
+const Com = () => <h1>类组件111</h1>
+
 // 定义组件的第二种方式 类组件 使用类继承React.Component/Component
 class App extends Component {      
     render(){
         console.log(this.props);
         return (
             <div>
-                <h1>类组件</h1>
+                <Com />
                 <p>{this.props.title}</p>
             </div>
         )
@@ -19,6 +22,7 @@ class App extends Component {
 //     title:"继承"
 // }).render()
 
+// render是react dom的一个方法， 这个方法一般只会调用一次 其他都可以是他的子组件
 render(
     <App  title="继承"/>,
     // app,
