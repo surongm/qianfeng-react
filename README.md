35f96d71c678721c4b618d8c73d471a6a5e35a64
surongm <1137747302@qq.com>
2019-11-03 15:21:12 +0800

    创建样式的 四种方式

--- README.md
+++ README.md
@@ -1,53 +1,29 @@
-## react
-react 官网  https://reactjs.org/
-（点击顶部菜单栏 可选择中文）
+(以下均为简洁版，要看复杂版请看前几个)
+## npm
+ npm 官网 
+  https://www.npmjs.com
 
-Facebook开源代码
-2013年
-用于构建用户界面的 JavaScript 库（不是框架）
+## 样式
 
-## react 
-创建一个 react项目 
-     npx create-react-app 01npx  
-或者 create-react-app 01npx
+① 行内样式
+    style={{color:'red',}}
+    或者 
+    const style = {color:'green'}
+    style={style}
 
+②外部样式
+    import './index.css'
 
-语法：
-JSX  JavaScript XML
+③动态创建样式
+    npm i classnames -D
+    import classNames from 'classnames'
+    className={classNames('a',{'b':true,'c':false})}
 
-
-## VSCode
-查看——外观——显示活动栏
-（最后一个为扩展）
-
-VSCode切换为中文  
-ctrl + shift + P  点击Configure Display Language
-或者直接点击 view下的EXTENSIONS  搜索chinese
-
-安装重启 即可
-
-禁用或者卸载什么什么 Formatter扩展程序
-
-代码格式化: Shift+Alt+F,或 Ctrl+Shift+P
-
-## react安装的扩展程序
-扩展中安装 ES7  
-ES7 React/Redux/GraphQL/React-Native snippets
- 一般写react 大家会安装这个~
-
-
-## react相关
-只要有index.js就可以运行起来~
-npm start
-访问http://localhost:3000/
-
-
-
-
-
-## 最最开始的准备工作
-先安装 node
-然后安装网上的教程 创建两个文件夹
-修改环境变量  添加系统变量NODE_HOME   是为了npm安装的包都下载到node_global/node_modules中
-修改系统变量path，是为了安装包（插件）以后可以直接使用 如cnpm  XXX
-安装cnpm是为了下载速度更快
+④样式组件
+    npm i styled-components -D
+    import styled from 'styled-components'
+    const Title = styled.h1`
+        color: yellow;
+        text-align:center
+    `
+    <Title>类组件</Title>
--- src/index.js
+++ src/index.js
@@ -1,75 +1,38 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import classNames from 'classnames'
+import styled from 'styled-components'
+import './index.css'
 
-// 这里是使用类的形式创建的组件，是jsx语法，但不是合理的js代码
-// class App extends Component {      
-//     render(){
-//         return (
-//             <div className="app" id="approot">
-//                 <h1 className="title">类组件</h1>
-//                 <p>继承</p>
-//             </div>
-//         )
-//     }
-// }
+console.log(styled)
 
-// jsx原理
-// 表示一个虚拟DOM树的方式
-// const app = {
-//     tag: "div",
-//     attrs: {
-//         className: "app",
-//         id: "approot"
-//     },
-//     children:[
-//         {
-//             tag: "h1",
-//             attrs: {
-//                 className: "title"
-//             },
-//             children: [
-//                 "类组件"
-//             ]
-//         },
-//         {
-//             tag: "p",
-//             attrs: null,
-//             children: [
-//                 "继承"
-//             ]
-//         }
-//     ]
-// }
-
-// 所以react在真正渲染的时候会把上面的类组件代码编辑为下面的代码，下面的代码是合法的js代码
 class App extends Component {      
     render(){
+        const style = {color:'green'}
+        const Title = styled.h1`
+            color: yellow;
+            text-align:center
+        `
         return (
-            // js语法  React.createElement是一个方法，用于创建元素  
-            // 可以有很多的参数 前两个是固定的 
-            // 第一个可以理解为标签名 
-            // 第二个可以理解为标签的属性 
-            // 剩下的可以继续写更多的子元素
-            // React.createElement(type,{props},[...children])
-            React.createElement(
-                "div",
-                {
-                    className: "app",
-                    id: "approot"
-                },
-                React.createElement(
-                    'h1',
-                    {
-                        className: "title"
-                    },
-                    "jsx原理"
-                ),
-                React.createElement(
-                    'p',
-                    null,
-                    "继承222"
-                )
-            )
+            <div>
+                {/* 第一个花括号是代表要写js语法 第二个花括号是指里面是一个对象 */}
+                <h1 style={{color:'red'}}>类组件</h1>
+                <ol>
+                    {/* 第一种样式 内联样式 */}
+                    <li style={style}>第二种方式 内联样式 类组件</li>
+                    {/* 第二种使用class  需要写成className */}
+                    <li className="text-color">第二种方式 外部样式 类组件</li>
+                    {/* 第三种方式 动态创建不同的className */}
+                    <li className={classNames('a',{'b':true,'c':false})}>
+                        第三种方式 动态创建不同的className 使用classnames
+                    </li>
+                    {/* 第四种方式  */}
+                    <li>
+                        第四种方式 使用styled-components  样式是一个组件
+                        <Title>类组件</Title>
+                    </li>
+                </ol>
+            </div>
         )
     }
 }
